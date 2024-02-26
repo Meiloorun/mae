c2e202f63337d7928705d5e84fe750d92b37b97d
Meiloorun <49651674+Meiloorun@users.noreply.github.com>
2024-02-26 14:53:52 +0000

    Update models_mae.py

--- models_mae.py
+++ models_mae.py
@@ -230,8 +230,8 @@ def mae_vit_base_patch16_dec512d8b(**kwargs):
 
 def mae_vit_large_patch16_dec512d8b(**kwargs):
     model = MaskedAutoencoderViT(
-        patch_size=16, embed_dim=1200, depth=24, num_heads=16,
-        decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+        patch_size=16, embed_dim=1024, depth=24, num_heads=20,
+        decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=20,
         mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
 
